bd6ba69e3eacc9ddb6c4e99a513e56ac0752e05e
Tarasov <tarasov@weelorum.com>
2023-02-11 14:19:00 +0200

    - created classes in core and core_android modules;

---
+++ core/src/main/java/com/example/core/utils/GlobalConstants.kt
@@ -0,0 +1,5 @@
+package com.example.core.utils
+
+object GlobalConstants {
+    const val emptyString = ""
+}
--- core_android/src/main/java/com/example/core_android/BaseViewModel.kt
+++ core_android/src/main/java/com/example/core_android/BaseViewModel.kt
@@ -7,10 +7,10 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 
 abstract class BaseViewModel<STATE, EVENT, EFFECT>: ViewModel() {
-    private val _state = MutableStateFlow(this.initialState())
+    protected val _state = MutableStateFlow(this.initialState())
     val state: StateFlow<STATE> = _state
 
-    private val _effect = MutableSharedFlow<EFFECT>()
+    protected val _effect = MutableSharedFlow<EFFECT>()
     val effect: SharedFlow<EFFECT> = _effect
 
     abstract fun initialState(): STATE
